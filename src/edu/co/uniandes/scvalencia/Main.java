37e08be438b6360a9b65fff383fc300812ade1d3
scvalencia <sc.valencia606@uniandes.edu.co>
2016-01-21 09:25:04 -0500

    New Java downloader of files

--- src/edu/co/uniandes/scvalencia/Main.java
+++ src/edu/co/uniandes/scvalencia/Main.java
@@ -1,6 +1,9 @@
 package edu.co.uniandes.scvalencia;
 
+import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class Main {
@@ -9,8 +12,14 @@ public class Main {
         URLRetriever urlr = new URLRetriever();
         List<String> subjectsURLS = urlr.getSubjectsScheduleURLS();
 
+        Date date = new Date() ;
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd.HH-mm-ss") ;
+        String path = "data/" + dateFormat.format(date);
+        File dir = new File(path);
+        dir.mkdir();
+
         for(String url : subjectsURLS) {
-            SubjectParser sp = new SubjectParser(url, true);
+            SubjectParser sp = new SubjectParser(url, true, path);
             sp.parseContent();
         }
     }
