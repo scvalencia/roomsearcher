1d5f0bb5f81a6450f531b3890a7ef752981a7506
scvalencia <sc.valencia606@uniandes.edu.co>
2016-01-21 09:25:39 -0500

    A simple script for file and data administration

---
+++ data/handler.py
@@ -0,0 +1,21 @@
+import os
+import subprocess
+from shutil import copyfile
+
+SCRIPT = 'prettify_json.rb'
+
+original_path = os.getcwd()
+all_subdirs = [d for d in os.listdir('.') if os.path.isdir(d)]
+latest_subdir = max(all_subdirs, key=os.path.getmtime)
+os.chdir(original_path + '/' + latest_subdir)
+
+copyfile(original_path + '/' + SCRIPT, os.getcwd() + '/' + SCRIPT)
+
+for f in os.listdir('.'):
+
+	if f != 'temp.json' and f != SCRIPT:	
+		os.popen('ruby prettify_json.rb %s > tmp.json' % f)
+		os.popen('cp tmp.json %s' % f)
+		os.popen('rm tmp.json')
+
+os.remove(SCRIPT)
