97543acde47f684b732cd3396fc279811af2b35d
scvalencia <sc.valencia606@uniandes.edu.co>
2016-01-21 09:32:37 -0500

    A simple script for file and data administration

--- data/handler.py
+++ data/handler.py
@@ -11,13 +11,18 @@ os.chdir(original_path + '/' + latest_subdir)
 
 copyfile(original_path + '/' + SCRIPT, os.getcwd() + '/' + SCRIPT)
 
+counter = 0
 for f in os.listdir('.'):
 
 	if f != 'temp.json' and f != SCRIPT:	
 		os.popen('ruby prettify_json.rb %s > tmp.json' % f)
 		os.popen('cp tmp.json %s' % f)
 		os.popen('rm tmp.json')
+
+	if counter % 8 == 0:
 		os.popen('git add %s' % f)
 		os.popen('git commit -m "New courses data for processing"')
+
+	couneter += 1
 
 os.remove(SCRIPT)
